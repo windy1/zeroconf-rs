11725994650646085762568368be18888f902427
Walker Crouse <Walker.Crouse@coop.co.uk>
2020-09-26 20:13:23 -0400

    refactor
    Signed-off-by: Walker Crouse <Walker.Crouse@coop.co.uk>

--- zeroconf/src/ffi/mod.rs
+++ zeroconf/src/ffi/mod.rs
@@ -5,6 +5,8 @@ use libc::{c_void, fd_set, timeval};
 use std::time::Duration;
 use std::{mem, ptr};
 
+pub mod cstr;
+
 /// Helper trait to convert a raw `*mut c_void` to it's rust type
 pub trait FromRaw<T> {
     /// Converts the specified `*mut c_void` to a `&'a mut T`.
@@ -64,66 +66,5 @@ pub unsafe fn read_select(sock_fd: i32, timeout: Duration) -> Result<u32> {
         Err("select(): returned error status".into())
     } else {
         Ok(result as u32)
-    }
-}
-
-pub mod cstr {
-    //! FFI utilities related to c-strings
-
-    use libc::c_char;
-    use std::ffi::CStr;
-
-    /// Returns the specified `*const c_char` as a `&'a str`. Ownership is not taken.
-    ///
-    /// # Safety
-    /// This function is unsafe due to a call to the unsafe function [`CStr::from_ptr()`].
-    ///
-    /// [`CStr::from_ptr()`]: https://doc.rust-lang.org/std/ffi/struct.CStr.html#method.from_ptr
-    pub unsafe fn raw_to_str<'a>(s: *const c_char) -> &'a str {
-        assert_not_null!(s);
-        CStr::from_ptr(s).to_str().unwrap()
-    }
-
-    /// Copies the specified `*const c_char` into a `String`.
-    ///
-    /// # Safety
-    /// This function is unsafe due to a call to the unsafe function [`raw_to_str()`].
-    ///
-    /// [`raw_to_str()`]: fn.raw_to_str.html
-    pub unsafe fn copy_raw(s: *const c_char) -> String {
-        assert_not_null!(s);
-        String::from(raw_to_str(s))
-    }
-
-    #[cfg(test)]
-    mod tests {
-        use super::*;
-        use libc::c_char;
-        use std::ptr;
-
-        #[test]
-        fn raw_to_str_success() {
-            let c_string = c_string!("foo");
-            unsafe { assert_eq!(raw_to_str(c_string.as_ptr() as *const c_char), "foo") };
-        }
-
-        #[test]
-        #[should_panic]
-        fn raw_to_str_expects_non_null() {
-            unsafe { raw_to_str(ptr::null() as *const c_char) };
-        }
-
-        #[test]
-        fn copy_raw_success() {
-            let c_string = c_string!("foo");
-            let c_str = c_string.as_ptr() as *const c_char;
-            unsafe { assert_eq!(copy_raw(c_str), "foo".to_string()) };
-        }
-
-        #[test]
-        #[should_panic]
-        fn copy_raw_expects_non_null() {
-            unsafe { copy_raw(ptr::null() as *const c_char) };
-        }
     }
 }
