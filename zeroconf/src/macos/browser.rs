373c71ba53f75798d79aa8e357165a8b110d351d
Oliver Koch <github@oltoko.de>
2021-01-07 19:45:28 +0100

    set protocol back to 0

--- zeroconf/src/macos/browser.rs
+++ zeroconf/src/macos/browser.rs
@@ -210,7 +210,7 @@ unsafe fn handle_resolve(
         GetAddressInfoParams::builder()
             .flags(bonjour_sys::kDNSServiceFlagsForceMulticast)
             .interface_index(interface_index)
-            .protocol(1)
+            .protocol(0)
             .hostname(host_target)
             .callback(Some(get_address_info_callback))
             .context(ctx.as_raw())
