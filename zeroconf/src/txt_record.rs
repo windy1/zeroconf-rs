b7154a70811efc8c2e01b946c5b652610a5161cb
Walker Crouse <Walker.Crouse@bjss.com>
2023-09-23 17:31:21 -0400

    relocate TXT record tests
    Signed-off-by: Walker Crouse <Walker.Crouse@bjss.com>

--- zeroconf/src/txt_record.rs
+++ zeroconf/src/txt_record.rs
@@ -141,6 +141,109 @@ impl Debug for TxtRecord {
 #[cfg(test)]
 mod tests {
     use super::*;
+    use crate::TxtRecord;
+    use std::collections::HashMap;
+
+    #[test]
+    fn insert_get_success() {
+        crate::tests::setup();
+        let mut record = TxtRecord::new();
+        record.insert("foo", "bar").unwrap();
+        assert_eq!(record.get("foo").unwrap(), "bar");
+        assert_eq!(record.get("baz"), None);
+    }
+
+    #[test]
+    fn remove_success() {
+        crate::tests::setup();
+        let mut record = TxtRecord::new();
+        record.insert("foo", "bar").unwrap();
+        record.remove("foo").unwrap();
+        assert!(record.get("foo").is_none());
+    }
+
+    #[test]
+    fn contains_key_success() {
+        crate::tests::setup();
+        let mut record = TxtRecord::new();
+        record.insert("foo", "bar").unwrap();
+        assert!(record.contains_key("foo"));
+        assert!(!record.contains_key("baz"));
+    }
+
+    #[test]
+    fn len_success() {
+        crate::tests::setup();
+        let mut record = TxtRecord::new();
+        record.insert("foo", "bar").unwrap();
+        assert_eq!(record.len(), 1);
+    }
+
+    #[test]
+    fn iter_success() {
+        crate::tests::setup();
+
+        debug!("iter_success()");
+
+        let mut record = TxtRecord::new();
+        record.insert("foo", "bar").unwrap();
+        record.insert("baz", "qux").unwrap();
+        record.insert("hello", "world").unwrap();
+
+        for (key, value) in record.iter() {
+            debug!("({:?}, {:?})", key, value);
+        }
+    }
+
+    #[test]
+    fn keys_success() {
+        crate::tests::setup();
+
+        let mut record = TxtRecord::new();
+        record.insert("foo", "bar").unwrap();
+        record.insert("baz", "qux").unwrap();
+        record.insert("hello", "world").unwrap();
+
+        for key in record.keys() {
+            debug!("{:?}", key);
+        }
+    }
+
+    #[test]
+    fn values_success() {
+        crate::tests::setup();
+
+        let mut record = TxtRecord::new();
+        record.insert("foo", "bar").unwrap();
+        record.insert("baz", "qux").unwrap();
+        record.insert("hello", "world").unwrap();
+
+        for value in record.values() {
+            debug!("{:?}", value);
+        }
+    }
+
+    #[test]
+    fn from_hashmap_success() {
+        crate::tests::setup();
+
+        let mut map = HashMap::new();
+        map.insert("foo", "bar");
+
+        let record: TxtRecord = map.into();
+
+        assert_eq!(record.get("foo").unwrap(), "bar");
+    }
+
+    #[test]
+    fn clone_success() {
+        crate::tests::setup();
+
+        let mut record = TxtRecord::new();
+        record.insert("foo", "bar").unwrap();
+
+        assert_eq!(record.clone(), record);
+    }
 
     #[test]
     fn serialize_success() {
