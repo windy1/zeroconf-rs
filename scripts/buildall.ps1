7129d3e8b2fa4a0f2a557ce47b606b61c861a1a6
Walker Crouse <walkercrouse@hotmail.com>
2024-08-19 11:41:01 -0400

    remove scripts directory (#61)
    Signed-off-by: Walker Crouse <walkercrouse@hotmail.com>

--- scripts/buildall.ps1
+++
@@ -1,8 +0,0 @@
-Set-PSDebug -Trace 1
-
-cargo build --workspace --verbose
-if ($LASTEXITCODE -ne 0) { Exit $LASTEXITCODE }
-
-cd examples
-cargo build --workspace --verbose
-if ($LASTEXITCODE -ne 0) { Exit $LASTEXITCODE }
